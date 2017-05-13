7ad557bd7505a6e3b06e5d0503fa0de00bf1c888
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-05-13 10:29:41 -0500

    Updated the flexbox, added more functions, and managed my state better.

--- public/js/main.js
+++ public/js/main.js
@@ -1,68 +1,81 @@
 let $ = window.jQuery
 
-$(document).ready(function () {
-// This checks to make sure the number isn't too long to fit in the sum box.
-  let checkNumLength = (number) => {
-    if (number.length > 9) {
-      sum.text(number.substr(number.length - 9, 9))
-      if (number.length > 15) {
-        number = ''
-        sum.text('Error')
-      }
-    }
-  }
+let calcExpressionArr = []
 
-  let number = ''
-  let newnum = ''
-  let operator = ''
-  let sum = $('#sum')
-  sum.text('0')
-
-// When any number on the calculator is clicked this makes 'number' set to the text of that button then sets 'sum' to that number.
-  $('.num').click(function () {
-    number += $(this).text()
-    sum.text(number)
-    checkNumLength(number)
-  })
-
-// When any of the operators are clicked this makes the 'operator' set to the text of that button then sets the first number that was clicked to 'newnum' so the second number clicked can be set to 'number'.
-  $('.operator').click(function () {
-    operator = $(this).text()
-    newnum = number
-    number = ''
-    sum.text('0')
-  })
+let number = ''
+let operator = ''
+let sum = $('#sum')
+sum.html('0')
 
 // When C is clicked all is cleared.
-  $('#clearall').click(function () {
-    number = ''
-    sum.text('0')
-    if ($(this).attr('id') === 'clearall') {
-      newnum = ''
-    }
-  })
+function clearSum () {
+  calcExpressionArr = []
+  number = ''
+  sum.html('0')
+}
 
 // When the decimal is clicked it appends a decimal to the number.
-  $('#decimal').click(() => {
-    number = number + '.'
-    sum.text(number)
-    checkNumLength(number)
-  })
+function appendDecimal () {
+  number = number + '.'
+  sum.html(number)
+}
 
-// When the equals is clicked it checks the operator being done then converts 'number' and 'newnum' from a string to an integer and does the operation. Then converts the answer back to a string to be able to put it inside the sum box.
-  $('#equals').click(() => {
-    if (operator === '+') {
-      number = +(parseFloat(number)) + +(parseFloat(newnum)).toString(10)
-    } else if (operator === '-') {
-      number = (parseFloat(newnum)) - parseFloat(number).toString(10)
-    } else if (operator === '/') {
-      number = (parseFloat(newnum)) / parseFloat(number).toString(10)
-    } else if (operator === 'x') {
-      number = (parseFloat(newnum)) * parseFloat(number).toString(10)
+function calculateSum () {
+  let total = calcExpressionArr.reduce(function (
+    accum, currentValue, index, arr) {
+    let newnum = parseFloat(arr[index + 1])
+    accum = parseFloat(accum)
+    if (currentValue === '+') {
+      accum += newnum
+    } else if (currentValue === '-') {
+      accum -= newnum
+    } else if (currentValue === 'x') {
+      accum *= newnum
+    } else if (currentValue === '/') {
+      accum /= newnum
     }
-    sum.text(number)
-    checkNumLength(number)
-    number = ''
-    newnum = ''
-  })
+    return accum
+  }, calcExpressionArr[0])
+  return total
+}
+
+$('#calc .num').click((e) => {
+  number += e.target.innerHTML
+  sum.html(number)
+  enableOperator()
 })
+
+$('#calc .operator').click((e) => {
+  operator = e.target.innerHTML
+  calcExpressionArr.push(number, operator)
+  sum.html(number + operator)
+  number = ''
+  disableOperator()
+})
+
+$('#equals').click(() => {
+  calcExpressionArr.push(number)
+  let total = calculateSum()
+  sum.html(total)
+  disableOperator()
+  calcExpressionArr = []
+  number = total
+  enableOperator()
+})
+
+$('#decimal').click(() => {
+  appendDecimal()
+})
+
+$('#clearall').click(() => {
+  clearSum()
+  enableOperator()
+})
+
+function disableOperator () {
+  $('.operator').attr('disabled', true)
+}
+
+function enableOperator () {
+  $('.operator').attr('disabled', false)
+}
